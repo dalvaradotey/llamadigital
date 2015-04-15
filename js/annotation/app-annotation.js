031f9b949d2af89806682bc4e20fb01dab790d40
dalvaradotey <d.alvaradotey@gmail.com>
2015-04-15 14:29:41 -0300

    TEST: block css

--- js/annotation/app-annotation.js
+++ js/annotation/app-annotation.js
@@ -1,7 +1,15 @@
 (function() {
+	// Google Web Fonts
     var link = document.createElement('link');
+    link.type = "text/css";
     link.rel = "stylesheet";
     link.href = "http://fonts.googleapis.com/css?family=Open+Sans:400,300,600,700";
+    document.querySelector("head").appendChild(link);
+    // Style 
+    var link = document.createElement('link');
+    link.type = "text/css";
+    link.rel = "stylesheet";
+    link.href = "css/style.css";
     document.querySelector("head").appendChild(link);
 })();
 /*! jQuery v1.11.1 | (c) 2005, 2014 jQuery Foundation, Inc. | jquery.org/license */
--- js/lib/init.js
+++ js/lib/init.js
@@ -1,6 +1,14 @@
 (function() {
+	// Google Web Fonts
     var link = document.createElement('link');
+    link.type = "text/css";
     link.rel = "stylesheet";
     link.href = "http://fonts.googleapis.com/css?family=Open+Sans:400,300,600,700";
+    document.querySelector("head").appendChild(link);
+    // Style 
+    var link = document.createElement('link');
+    link.type = "text/css";
+    link.rel = "stylesheet";
+    link.href = "css/style.css";
     document.querySelector("head").appendChild(link);
 })();
