f19f0a7968b7cf7dcf96bf2fcc0c7347258e3f04
dalvaradotey <d.alvaradotey@gmail.com>
2015-04-15 14:25:58 -0300

    TEST: block css

---
+++ js/lib/init.js
@@ -0,0 +1,6 @@
+(function() {
+    var link = document.createElement('link');
+    link.rel = "stylesheet";
+    link.href = "http://fonts.googleapis.com/css?family=Open+Sans:400,300,600,700";
+    document.querySelector("head").appendChild(link);
+})();
