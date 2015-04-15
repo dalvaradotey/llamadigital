a7311840a2c06d75f9b809dbc03ed57756ebc842
dalvaradotey <d.alvaradotey@gmail.com>
2015-04-15 14:43:32 -0300

    UPDATE: servicios

--- js/lib/init.js
+++ js/lib/init.js
@@ -5,10 +5,4 @@
     link.rel = "stylesheet";
     link.href = "http://fonts.googleapis.com/css?family=Open+Sans:400,300,600,700";
     document.querySelector("head").appendChild(link);
-    // Style 
-    var link = document.createElement('link');
-    link.type = "text/css";
-    link.rel = "stylesheet";
-    link.href = "css/style.css";
-    document.querySelector("head").appendChild(link);
 })();
